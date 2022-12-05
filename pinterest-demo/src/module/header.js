809a348b80041f2cb29574dda07b8b42a5804f3e
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2022-12-05 22:12:41 +0330

    setup folders

---
+++ pinterest-demo/src/module/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../style/header.module";
+
+class header extends Component {
+    render() {
+        return (
+            <>
+              <div className='box'></div>  
+            </>
+        );
+    }
+}
+
+export default header;
---
+++ pinterest-demo/src/module/pin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../style/pin.module";
+
+class pin extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default pin;
